349e6c3a37a35cab9c836564405de9f359fd0d5a
Manuel Prieto <m.prietoh@uniandes.edu.co>
2024-04-14 16:40:38 -0500

    commid final

--- main.ts
+++ main.ts
@@ -12,14 +12,31 @@ renderSeriesInTable(series);
 seassonAverageElm.innerHTML = `${getAverageSeasson(series)}`
 
 function renderSeriesInTable(series: Serie[]): void {
-    series.forEach(c => {
-        let trElement = document.createElement("tr");
-        trElement.innerHTML = `<td>${c.id}</td>
-                                <td>${c.name}</td>
-                                <td>${c.channel}</td>
-                                <td>${c.seasons}</td>`;
-        seriesTbody.appendChild(trElement);
+    series.forEach(serie => {
+      let trElement = document.createElement("tr");
+      trElement.innerHTML = `<td>${serie.id}</td>
+                              <td>${serie.name}</td>
+                              <td>${serie.channel}</td>
+                              <td>${serie.seasons}</td>`;
+      trElement.addEventListener('click', () => showDetails(serie));
+      seriesTbody.appendChild(trElement);
     });
+}  
+
+function showDetails(serie: Serie): void {
+        const detailsSection = document.getElementById('details-section');
+        if (detailsSection) {
+            detailsSection.innerHTML = `
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="${serie.picture}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">${serie.name}</h5>
+                        <p class="card-text">${serie.description}</p>
+                        <a href="${serie.link}" class="btn btn-primary">Go to series</a>
+                    </div>
+                </div>
+            `;
+        }
 }
 
 function getAverageSeasson(series: Serie[]): number {
--- scripts/main.js
+++ scripts/main.js
@@ -6,11 +6,18 @@ var seassonAverageElm = document.getElementById("seasson-average");
 renderSeriesInTable(series);
 seassonAverageElm.innerHTML = "".concat(getAverageSeasson(series));
 function renderSeriesInTable(series) {
-    series.forEach(function (c) {
+    series.forEach(function (serie) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(c.id, "</td>\n                                <td>").concat(c.name, "</td>\n                                <td>").concat(c.channel, "</td>\n                                <td>").concat(c.seasons, "</td>");
+        trElement.innerHTML = "<td>".concat(serie.id, "</td>\n                              <td>").concat(serie.name, "</td>\n                              <td>").concat(serie.channel, "</td>\n                              <td>").concat(serie.seasons, "</td>");
+        trElement.addEventListener('click', function () { return showDetails(serie); });
         seriesTbody.appendChild(trElement);
     });
+}
+function showDetails(serie) {
+    var detailsSection = document.getElementById('details-section');
+    if (detailsSection) {
+        detailsSection.innerHTML = "\n                <div class=\"card\" style=\"width: 18rem;\">\n                    <img class=\"card-img-top\" src=\"".concat(serie.picture, "\" alt=\"Card image cap\">\n                    <div class=\"card-body\">\n                        <h5 class=\"card-title\">").concat(serie.name, "</h5>\n                        <p class=\"card-text\">").concat(serie.description, "</p>\n                        <a href=\"").concat(serie.link, "\" class=\"btn btn-primary\">Go to series</a>\n                    </div>\n                </div>\n            ");
+    }
 }
 function getAverageSeasson(series) {
     var averageSeasson = 0;
