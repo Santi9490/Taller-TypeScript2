f310098218a745a6320a0dcb05843086f5704ab4
Manuel Prieto <m.prietoh@uniandes.edu.co>
2024-04-14 16:23:55 -0500

    cambios

--- serie.ts
+++ serie.ts
@@ -1,24 +1,7 @@
-<<<<<<< HEAD
 export class Serie {
   
   constructor(public id: number, public name: string, public channel: string, public seasons: number, public description: string, public link: string, public picture: string) {
   
 }
 }
-
-
-
-  
-=======
-export class Serie {
-  
-  constructor(public id: number, public name: string, public channel: string, public seasons: number, public description: string, public link: string, public picture: string) {
-  
-}
-}
-
-
-
-  
->>>>>>> 7c5efa22fcb074fed58dd4b12be9a2aca35b3bf0
   
